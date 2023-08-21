7458f595d3a61f8a20fffdc2376c0a4d32aac9a1
sunyzm <mingzh26@gmail.com>
2023-08-21 10:05:27 -0700

    Add Flask to requirements.txt

--- gquery/gquery_lib_test.py
+++ gquery/gquery_lib_test.py
@@ -10,13 +10,13 @@ flags.DEFINE_string("data_path", None, "Path to data files.")
 def main(argv):
     del argv  # Unused.
 
+    # Check the data_path has a trailing backslash.
     if FLAGS.data_path[::-1] != "/":
         FLAGS.data_path += "/"
 
     file_path = FLAGS.data_path + "worldcities.csv"
     if not os.path.exists(file_path):
         raise FileExistsError(f"File '{file_path}' does not exists")
-
 
     query_engine = GQueryEngine(file_path)
     query_engine.print("New York")
