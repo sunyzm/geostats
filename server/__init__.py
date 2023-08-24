3fdabc828702a23f51ecf132b1ed501f9f66b67b
sunyzm <mingzh26@gmail.com>
2023-08-23 17:18:33 -0700

    Create skeleton show_city_info function.

--- server/__init__.py
+++ server/__init__.py
@@ -17,5 +17,10 @@ def create_app(test_config=None):
     @app.route("/index.html")
     def index():
         return render_template("index.html")
+    
+    @app.route("/id/<int:city_id>")
+    def show_city_info(city_id):
+        city_info = {"name": "San Francisco", "population": 900000, "country": "USA"}
+        return render_template("city.html", city_info=city_info)
 
     return app
