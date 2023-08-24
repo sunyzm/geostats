465a0b6acfcc5171bfec399d94ff2a25eaa850a9
sunyzm <mingzh26@gmail.com>
2023-08-23 20:50:58 -0700

    Import gquery_lib

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,6 @@
-import os
-
 from flask import Flask, render_template
+import os
+import sys
 
 
 def create_app(test_config=None):
@@ -13,11 +13,21 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "gquery")
+    )
+    from gquery_lib import GQueryEngine
+
+    datafile = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), os.pardir, "data/worldcities.csv"
+    )
+    query_engine = GQueryEngine(datafile)
+
     @app.route("/")
     @app.route("/index.html")
     def index():
         return render_template("index.html")
-    
+
     @app.route("/id/<int:city_id>")
     def show_city_info(city_id):
         city_info = {"name": "San Francisco", "population": 900000, "country": "USA"}
